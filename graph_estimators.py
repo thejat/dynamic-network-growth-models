4eddee45d012c5799542fafde4bfd0ba11676a86
Theja <theja2t@gmail.com>
2017-05-30 16:22:11 -0500

    minor

--- graph_estimators.py
+++ graph_estimators.py
@@ -1,1 +1,54 @@
 #graph parameter estimation
+
+def estimate_random_dynamic_no_arrival(GT):
+
+	Kmax = 1000
+	p = 0.5
+
+
+	n_nodes = len(GT[0].nodes())
+	t1,t2,t3,t4,t5 = 0,0,0,0,0
+	for j in range(n_nodes):
+		for i in range(0,j):
+
+			if (i,j) in GT[0].edges():
+				t1 += 1
+
+			t2temp = 0
+			for t in range(1,len(GT)):
+				if (i,j) not in GT[t-1].edges() and (i,j) in GT[t].edges():
+					t2temp += 1
+			t2 +=t2temp
+
+			t3temp = 0
+			for t in range(1,len(GT)):
+				if (i,j) not in GT[t-1].edges():
+					t3temp += 1
+			t3 +=t3temp
+
+			t4temp = 0			
+			for t in range(1,len(GT)):
+				if (i,j) in GT[t-1].edges() and (i,j) not in GT[t].edges():
+					t4temp += 1
+			t4 +=t4temp
+
+			t5temp = 0
+			for t in range(1,len(GT)):
+				if (i,j) in GT[t-1].edges():
+					t5temp += 1
+			t5 +=t5temp
+
+
+
+	pterm = n_nodes*(n_nodes-1)*.5*p
+	for k in range(Kmax):
+		alpha = (t1 - pterm +t3)/(t1 - pterm +t3)
+		pterm = n_nodes*(n_nodes-1)*.5*(alpha/(alpha+beta))
+		beta = (pterm - t1 + t4)/(pterm -t1 + t5)
+		pterm = n_nodes*(n_nodes-1)*.5*(alpha/(alpha+beta))
+
+
+
+	return alpha,beta
+
+# estimate_random_dynamic_no_arrival(GT)
--- graph_generators.py
+++ graph_generators.py
@@ -1,29 +1,34 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
+from graph_estimators import estimate_random_dynamic_no_arrival
+import time
 
 
 
-def generate_random_dynamic_graphs(alpha=0.5,beta=0.6):
+def generate_random_dynamic_graphs(alpha=0.5,beta=0.6,n0=50,flag_draw=False,total_time=10,flag_arrivals=True):
 
-	G=nx.Graph()
 
-	alpha = .5
-	beta = .6
-
-	G.add_nodes_from(range(5))
-	for i in range(0,5):
-		for j in range(0,i):
+	#Graph at time zero
+	Goriginal=nx.Graph()
+	Goriginal.add_nodes_from(range(n0))
+	for j in range(n0):
+		for i in range(0,j):
 			if np.random.rand() > alpha/(alpha+beta):
-				G.add_edge(i,j)
-	nx.draw(G,pos=nx.spring_layout(G))  # networkx draw()
-	plt.show()  # pyplot draw()
+				Goriginal.add_edge(i,j)
+	if flag_draw==True:
+		nx.draw(Goriginal,pos=nx.spring_layout(Goriginal))  # networkx draw()
+		plt.show()  # pyplot draw()
 
-	GT = [G]
-	for t in range(1,3): #t = 1,2
+
+	#Subsequent graphs
+	GT = [Goriginal]
+	for t in range(1,total_time+1): #t = 1,2,...,t
+
 		Gcurrent = GT[t-1].copy()
 		current_nodes = sorted(Gcurrent.nodes())
 
+		#additions/deletions with existing nodes
 		for j in current_nodes:
 			for i in current_nodes:
 				if i < j:
@@ -34,19 +39,34 @@ def generate_random_dynamic_graphs(alpha=0.5,beta=0.6):
 						if np.random.rand() > alpha/(alpha+beta):
 							Gcurrent.add_edge(i,j)
 
-		new_node_index = len(current_nodes)+1
-		Gcurrent.add_node(new_node_index)
-		for i in current_nodes:
-			if np.random.rand() > alpha/(alpha+beta):
-				Gcurrent.add_edge(i,new_node_index)
+		if flag_arrivals==True:
+			#new node arrives #tbd: generalize to Z_t nodes arriving
+			new_node_index = len(current_nodes)+1
+			Gcurrent.add_node(new_node_index)
+			for i in current_nodes:
+				if np.random.rand() > alpha/(alpha+beta):
+					Gcurrent.add_edge(i,new_node_index)
 
 		GT.append(Gcurrent)
 
-		nx.draw(Gcurrent,pos=nx.spring_layout(Gcurrent))  # networkx draw()
-		plt.show()  # pyplot draw()
+		if flag_draw==True:
+			nx.draw(Gcurrent,pos=nx.spring_layout(Gcurrent))  # networkx draw()
+			plt.show()  # pyplot draw()
 
 
-		return GT
+	return GT
 
 if __name__=='__main__':
-	GT = generate_random_dynamic_graphs()
+
+	alphaTrue = 0.3
+	betaTrue = 0.9
+
+	st = time.time()
+	GT = generate_random_dynamic_graphs(alpha=alphaTrue,beta=betaTrue,total_time=20,flag_arrivals=False)
+	print "generated data, time taken:",time.time() - st
+	st = time.time() 
+	alpha,beta = estimate_random_dynamic_no_arrival(GT)
+	print "estimated from data, time taken: ",time.time() - st
+
+	print "True vals: alpha",alphaTrue," beta",betaTrue
+	print "Estimates: alpha",alpha," beta ",beta

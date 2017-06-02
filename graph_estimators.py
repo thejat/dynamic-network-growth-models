beda95cb5b2b5748ce226fbd9a484b07f75541f2
Theja <theja2t@gmail.com>
2017-06-02 18:00:17 -0500

    bug arrival estimation

--- graph_estimators.py
+++ graph_estimators.py
@@ -1,6 +1,7 @@
 #graph parameter estimation
 import numpy as np
-import pprint
+import pprint, time
+import networkx as nx
 
 
 def get_statistics(GT):
@@ -68,20 +69,134 @@ def estimate_random_dynamic_no_arrival_gridsearch(GT):
 	t1,t2,t3,t4,t5 = meta['t1'],meta['t2'],meta['t3'],meta['t4'],meta['t5']
 	eps = 1e-4
 	n_nodes = len(GT[0].nodes())
-	gridvals = np.linspace(eps,1-eps,6)
+	gridvals = np.linspace(eps,1-eps,10)
 	L = np.zeros((len(gridvals),len(gridvals)))
 	maxidx = [0,0]
 	for i,alpha in enumerate(gridvals):
 		for j,beta in enumerate(gridvals):
 			L[i,j] = round(t1*np.log(alpha/(alpha+beta)) + (n_nodes*(n_nodes-1)*.5 - t1)*np.log(beta/(alpha+beta))	\
-				+t2*np.log(alpha) + (t2-t3)*np.log(1-alpha) + t4*np.log(beta) + (t5-t4)*np.log(1-beta),2)
+				+t2*np.log(alpha) + (t3-t2)*np.log(1-alpha) + t4*np.log(beta) + (t5-t4)*np.log(1-beta),2)
 
 			if L[i,j] > L[maxidx[0],maxidx[1]]:
 				maxidx[0],maxidx[1] = i,j
 
 	print np.round(gridvals,2)
-	pprint.pprint(L)
+	# pprint.pprint(L)
 	print 'best alpha',gridvals[maxidx[0]]
 	print 'best beta',gridvals[maxidx[1]]
 
 	return gridvals[maxidx[0]],gridvals[maxidx[1]]
+
+
+def get_statistics_arrivals(GT):
+
+	AT = [nx.adjacency_matrix(G) for G in GT]
+
+	#lambda = (termA -nC2p + termB + termC - sumt(nptC2)p) / ( termD - nC2p + termE + termF - sumt(nptC2)p)  )
+	
+	termlambda = {x:0 for x in 'ABCDEF'}
+
+	#term A and D
+	n0 = len(GT[0].nodes())
+	for j in range(n0):
+		for i in range(0, j):
+			termlambda['A'] += AT[0][i,j]
+
+	termlambda['D'] = termlambda['A']
+
+	#term B and E
+	for t in range(1,len(GT)):
+		nptminus1 = len(GT[t-1].nodes())
+		for j in range(nptminus1):
+			for i in range(0, j):
+				termlambda['B'] += (1-AT[t-1][i,j])*AT[t][i,j]
+
+				termlambda['E'] += 1-AT[t-1][i,j]
+
+	#terms C and F
+	for t in range(1, len(GT)):
+		npt = len(GT[t].nodes())
+		for j in range(npt):
+			for i in range(0, j):
+				termlambda['C'] += AT[t][i,j]
+
+	termlambda['F'] = termlambda['C']
+
+
+
+	#mu = (termU -nC2p + termV + termW - sumt(nptC2)p) / ( termX - nC2p + termY + termZ - sumt(nptC2)p)  )
+
+	termmu = {x:0 for x in 'UVWXYZ'}
+
+	#termmu U and X
+	n0 = len(GT[0].nodes())
+	for j in range(n0):
+		for i in range(0, j):
+			termmu['U'] += (-AT[0][i,j])
+
+	termmu['X'] = termmu['U']
+
+	#termmu V and Y
+	for t in range(1,len(GT)):
+		nptminus1 = len(GT[t-1].nodes())
+		for j in range(nptminus1):
+			for i in range(0, j):
+				termmu['V'] += AT[t-1][i,j]*(1 - AT[t][i,j])
+
+				termmu['Y'] += AT[t-1][i,j]
+
+	#termmu W and Z
+	for t in range(1, len(GT)):
+		npt = len(GT[t].nodes())
+		for j in range(npt):
+			for i in range(0, j):
+				termmu['W'] += (-AT[t][i,j])
+
+	termmu['Z'] = termmu['W']
+
+
+	coeff_nC2p = len(GT[0].nodes())*(len(GT[0].nodes()) - 1)*0.5
+	coeff_sumt_nptC2p = 0
+	for t in range(1, len(GT)):
+		npt = len(GT[t].nodes())
+		for j in range(npt):
+			for i in range(0, j):
+				coeff_sumt_nptC2p += 1
+
+
+	return {'termlambda':termlambda,'termmu':termmu,'coeff_p':[coeff_nC2p,coeff_sumt_nptC2p]}
+
+
+def estimate_random_dynamic_with_arrival_recursive(GT):
+
+
+	st = time.time()
+	print "Estimating parameters"
+
+	meta = get_statistics_arrivals(GT)
+	print meta['termlambda']
+	print meta['termmu']
+	print meta['coeff_p']
+
+	Kmax = 1
+
+	p = 0.5 #initialize
+	for k in range(Kmax):
+		lmbd = (meta['termlambda']['A'] - meta['coeff_p'][0]*p \
+				+meta['termlambda']['B'] + meta['termlambda']['C'] - meta['coeff_p'][1]*p)\
+				/(meta['termlambda']['D'] - meta['coeff_p'][0]*p \
+				+meta['termlambda']['E'] + meta['termlambda']['F'] - meta['coeff_p'][1]*p)
+
+		mu = (meta['termmu']['U'] - meta['coeff_p'][0]*p \
+				+meta['termmu']['V'] + meta['termmu']['W'] - meta['coeff_p'][1]*p)\
+				/(meta['termmu']['X'] - meta['coeff_p'][0]*p \
+				+meta['termmu']['Y'] + meta['termmu']['Z'] - meta['coeff_p'][1]*p)
+
+		print 'mu',mu
+		
+
+		p = lmbd/(lmbd+mu)
+
+	print "\t Time taken: ",time.time() - st
+
+	return lmbd,mu
--- graph_generators.py
+++ graph_generators.py
@@ -2,20 +2,22 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 np.random.seed(1000)
-from graph_estimators import estimate_random_dynamic_no_arrival_recursive,estimate_random_dynamic_no_arrival_gridsearch
+from graph_estimators import estimate_random_dynamic_no_arrival_recursive,estimate_random_dynamic_with_arrival_recursive
 import time
 
 
 
 def generate_random_dynamic_graphs(alpha=0.5,beta=0.6,n0=50,flag_draw=False,total_time=10,flag_arrivals=True):
 
+	st = time.time()
+	print "Generating data"
 
 	#Graph at time zero
 	Goriginal=nx.Graph()
 	Goriginal.add_nodes_from(range(n0))
 	for j in range(n0):
 		for i in range(0,j):
-			if np.random.rand() > alpha/(alpha+beta):
+			if np.random.rand() <= alpha/(alpha+beta):
 				Goriginal.add_edge(i,j)
 	if flag_draw==True:
 		nx.draw(Goriginal,pos=nx.spring_layout(Goriginal))  # networkx draw()
@@ -26,6 +28,8 @@ def generate_random_dynamic_graphs(alpha=0.5,beta=0.6,n0=50,flag_draw=False,tota
 	GT = [Goriginal]
 	for t in range(1,total_time+1): #t = 1,2,...,t
 
+		print "\tGraph snapshot at snapshot",t, " time",time.time()-st
+
 		Gcurrent = GT[t-1].copy()
 		current_nodes = sorted(Gcurrent.nodes())
 
@@ -34,10 +38,10 @@ def generate_random_dynamic_graphs(alpha=0.5,beta=0.6,n0=50,flag_draw=False,tota
 			for i in current_nodes:
 				if i < j:
 					if (i,j) in Gcurrent.edges():
-						if np.random.rand() > beta/(alpha+beta):
+						if np.random.rand() <= beta/(alpha+beta):
 							Gcurrent.remove_edge(i,j)
 					else:
-						if np.random.rand() > alpha/(alpha+beta):
+						if np.random.rand() <= alpha/(alpha+beta):
 							Gcurrent.add_edge(i,j)
 
 		if flag_arrivals==True:
@@ -45,7 +49,7 @@ def generate_random_dynamic_graphs(alpha=0.5,beta=0.6,n0=50,flag_draw=False,tota
 			new_node_index = len(current_nodes)+1
 			Gcurrent.add_node(new_node_index)
 			for i in current_nodes:
-				if np.random.rand() > alpha/(alpha+beta):
+				if np.random.rand() <= alpha/(alpha+beta):
 					Gcurrent.add_edge(i,new_node_index)
 
 		GT.append(Gcurrent)
@@ -54,28 +58,39 @@ def generate_random_dynamic_graphs(alpha=0.5,beta=0.6,n0=50,flag_draw=False,tota
 			nx.draw(Gcurrent,pos=nx.spring_layout(Gcurrent))  # networkx draw()
 			plt.show()  # pyplot draw()
 
+	print "\tTime taken:",time.time() - st
 
 	return GT
 
 if __name__=='__main__':
 
-	alphaTrue = 0.3
-	betaTrue = 0.9
+	# alphaTrue = 0.7
+	# betaTrue = 0.4
 
-	st = time.time()
-	GT = generate_random_dynamic_graphs(alpha=alphaTrue,beta=betaTrue,total_time=3,flag_arrivals=False)
-	print "generated data, time taken:",time.time() - st
+	# st = time.time()
+	# GT = generate_random_dynamic_graphs(alpha=alphaTrue,beta=betaTrue,n0=20,flag_draw=False,total_time=1000,flag_arrivals=False)
+	# print "generated data, time taken:",time.time() - st
 
-	st = time.time() 	
-	alpha,beta = estimate_random_dynamic_no_arrival_recursive(GT)
-	print "estimated from data, time taken: ",time.time() - st
-	print "True vals: alpha",alphaTrue," beta",betaTrue
-	print "Estimates: alpha",alpha," beta ",beta
+	# st = time.time() 	
+	# alpha,beta = estimate_random_dynamic_no_arrival_recursive(GT)
+	# print "estimated from data, time taken: ",time.time() - st
+	# print "True vals: alpha",alphaTrue," beta",betaTrue
+	# print "Estimates: alpha",alpha," beta ",beta
 	
-	st = time.time() 
-	alpha,beta = estimate_random_dynamic_no_arrival_gridsearch(GT)
-	print "estimated from data, time taken: ",time.time() - st
-	print "True vals: alpha",alphaTrue," beta",betaTrue
-	print "Estimates: alpha",alpha," beta ",beta
+	# st = time.time()
+	# alpha,beta = estimate_random_dynamic_no_arrival_gridsearch(GT)
+	# print "estimated from data, time taken: ",time.time() - st
+	# print "True vals: alpha",alphaTrue," beta",betaTrue
+	# print "Estimates: alpha",alpha," beta ",beta
 
 
+
+	lmbdTrue = 0.7
+	muTrue = 0.4
+
+	GT = generate_random_dynamic_graphs(alpha=lmbdTrue,beta=muTrue,n0=20,flag_draw=False,total_time=10,flag_arrivals=True)
+	
+	lmbd,mu = estimate_random_dynamic_with_arrival_recursive(GT)
+
+	print "True vals: lambda",lmbdTrue," mu",muTrue
+	print "Estimates: lambda",lmbd," mu ",mu

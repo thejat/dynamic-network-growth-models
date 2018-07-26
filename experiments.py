a1b4f69841106680d20eaa52393cca50a130475a
Theja <tt@theja.org>
2018-07-26 16:33:16 -0500

    unify output vs input

--- experiments.py
+++ experiments.py
@@ -4,16 +4,15 @@ from experiments_base import *
 if __name__=='__main__':
 
 	params = get_params() 	#common parameters
-	params['multiprocessing'] 	= False
+	params['multiprocessing'] 	= True
 	params['dynamic'] 		= 'lazy'
 	params['only_unify'] 	= True
 	params['compare_unify'] = True
-	params['n'] 			= 100
-	params['total_time']	= 32
+	params['n'] 			= 50
+	params['total_time']	= 128
 	params['estimation_indices'] = [int(math.pow(2,i))+1 for i in range(1,int(math.log2(params['total_time']))+1)]
 	params['Mutrue'] 		= np.array([[.8,.2,.1,.1],[.2,.8,.2,.2],[.1,.2,.8,.2],[.1,.2,.2,.8]])# [bernoulli]
-	params['Wtrue'] 		= np.array([[.8,.2,.1,.1],[.2,.8,.2,.2],[.1,.2,.8,.2],[.1,.2,.2,.8]])
-	# params['Wtrue'] 		= np.array([[.4,.2,.1,.1],[.2,.4,.2,.2],[.1,.2,.4,.2],[.1,.2,.2,.4]])
+	params['Wtrue'] 		= np.array([[.7,.2,.1,.1],[.2,.7,.2,.2],[.1,.7,.4,.2],[.1,.2,.2,.7]])
 	params['k'] 			= params['Wtrue'].shape[0]
 
 
